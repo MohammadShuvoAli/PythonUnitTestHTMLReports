5109bdb6bda92f8789698d6f7c9afa9c0b16cac8
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-07 17:24:42 +0600

    update

---
+++ test_orange_hrm_login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import unittest
+import HtmlTestRunner
+from selenium.webdriver.common.by import By
+
+
+class OrangeHRMTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+
+    def test_homePageTitle(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.assertEqual("OrangeHRM", self.driver.title, "webpage title did not match")
+        self.driver.implicitly_wait(10)
+
+    def test_login(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("Admin")
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        self.assertEqual("OrangeHRM", self.driver.title, "webpage title did not match")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        print("Test Completed......")
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="..//Reports"))
